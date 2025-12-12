121758b8f4e01b7af74fe77e81b46ca828d558fa
ggauravp <gauravpant870@gmail.com>
2025-12-12 07:40:51 +0000

    secrets to oidc

--- iam.tf
+++ iam.tf
@@ -71,6 +71,9 @@ resource "aws_iam_role" "github_actions" {
       }
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
         StringLike = {
           "token.actions.githubusercontent.com:sub" = [
             "repo:ggauravp/terraform-cicd:ref:refs/heads/main",

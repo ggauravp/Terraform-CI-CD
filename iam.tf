72f2162535e0f1abef0819034cab84598d27778b
ggauravp <gauravpant870@gmail.com>
2025-12-12 08:14:31 +0000

    aaa

--- iam.tf
+++ iam.tf
@@ -76,8 +76,8 @@ resource "aws_iam_role" "github_actions" {
         }
         StringLike = {
           "token.actions.githubusercontent.com:sub" = [
-            "repo:ggauravp/terraform-cicd:ref:refs/heads/main",
-            "repo:ggauravp/terraform-cicd:ref:refs/pull/*/merge"
+            "repo:ggauravp/Terraform-CI-CD:ref:refs/heads/main",
+            "repo:ggauravp/Terraform-CI-CD:ref:refs/pull/*/merge"
           ]
         }
       }

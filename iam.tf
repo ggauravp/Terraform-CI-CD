d1f88b1e1d180e5820628ddea36a67c65432b01b
ggauravp <gauravpant870@gmail.com>
2025-12-12 06:30:42 +0000

    secrets to oidc

--- iam.tf
+++ iam.tf
@@ -57,3 +57,64 @@ resource "aws_iam_role_policy_attachment" "attach" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
+
+# create oidc provider for github actions
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+# create iam role for github actions
+resource "aws_iam_role" "github_actions"{
+  name = "github-actions-oidc-role"
+   assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:ggauravp/terraform-cicd:*"
+        }
+      }
+    }]
+   })
+}
+
+resource "aws_iam_policy" "github_actions_policy" {
+  name = "github-actions-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:*",
+          "ec2:*",
+          "iam:*",
+          "lambda:*",
+          "cloudwatch:*",
+          "logs:*",
+          "vpc:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_attach" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions_policy.arn
+  
+}
